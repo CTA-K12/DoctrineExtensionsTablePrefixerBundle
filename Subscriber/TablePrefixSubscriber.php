0e7beff13cbb8a58fc7b3d9a79cf6527677b5868
Tyler Hand <thand@mesd.k12.or.us>
2014-09-19 09:08:17 -0700

    Added the flatten option to replace the schemas with single table name

--- Subscriber/TablePrefixSubscriber.php
+++ Subscriber/TablePrefixSubscriber.php
@@ -1,72 +1,158 @@
 <?php
 namespace Mesd\DoctrineExtensions\TablePrefixerBundle\Subscriber;
 
+use Doctrine\Common\EventSubscriber;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadata;
 
-class TablePrefixSubscriber implements \Doctrine\Common\EventSubscriber
+class TablePrefixSubscriber implements EventSubscriber
 {
-    private $bundles;
-    private $defaults;
+    /**
+     * Array of options for each bundle
+     * @var array
+     */
+    protected $bundles;
 
-    public function __construct( $container ) {
-        if ( $container->hasParameter( 'prefixed_bundles' ) ) {
-            $this->bundles = $container->getParameter( 'prefixed_bundles' ) ;
+    /**
+     * Flag pertaining to whether the defaults were set or not
+     * @var boolean
+     */
+    protected $defaultsSet;
+
+    /**
+     * Array of default options
+     * @var array
+     */
+    protected $defaults;
+
+
+    /**
+     * Consturctor
+     *
+     * @param ContainerInterface $container Reference to symfonys container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        if ($container->hasParameter('prefixed_bundles')) {
+            $this->bundles = $container->getParameter('prefixed_bundles');
             $this->defaults = null;
-            if ( isset( $this->bundles['default'] ) ) { $this->defaults=$this->bundles['default'];}
-        } else {
-
+            if (isset($this->bundles['default'])) {
+                $this->defaults = $this->bundles['default'];
+                if (!isset($this->defaults['flatten'])) {
+                    $this->defaults['flatten'] = false;
+                }
+                $this->defaultsSet = true;
+            } else {
+                $this->defaultsSet = false;
+            }
         }
     }
 
-    public function getSubscribedEvents() {
-        return array( 'loadClassMetadata' , 'prePersist' );
+
+    /**
+     * Get a list of the events this listener subscribes to
+     *
+     * @return array List of events subscribed to
+     */
+    public function getSubscribedEvents()
+    {
+        return array('loadClassMetadata' , 'prePersist');
     }
 
 
-    public function prePersist( LifecycleEventArgs $eventArgs) {
-        $classMetadata=$eventArgs->getEntityManager()
+    /**
+     * Action to call on the prepersist event
+     *
+     * @param  LifecycleEventArgs $eventArgs The event arguments
+     */
+    public function prePersist(LifecycleEventArgs $eventArgs)
+    {
+        $classMetadata = $eventArgs->getEntityManager()
             ->getClassMetadata(get_class($eventArgs->getEntity()));
         $this->prefix($classMetadata);
     }
 
-    public function loadClassMetadata( LoadClassMetadataEventArgs $eventArgs ) {
+
+    /**
+     * Action to call on the load class metadata event
+     *
+     * @param  LoadClassMetadataEventArgs $eventArgs The event arguments
+     */
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
         $classMetadata = $eventArgs->getClassMetadata();
         $this->prefix($classMetadata);
     }
 
-    public function prefix($classMetadata){
-        $blank=( 'blank' == $this->defaults['schema'] );
-        $bundle=strstr( $classMetadata->getName(), 'Bundle\\', True ).'Bundle';
-        $prefix=isset( $this->bundles[$bundle]['prefix'] )?$this->bundles[$bundle]['prefix']:null;
-        $schema=isset( $this->bundles[$bundle]['schema'] )?$this->bundles[$bundle]['schema']:null;
-        $sequenceGeneratorDefinition=$classMetadata->sequenceGeneratorDefinition;
 
-        if ( $this->defaults || !isset( $this->bundles[$bundle] ) ) {
-            $prefix=( is_null( $prefix )?$this->defaults['prefix']:$prefix );
-            $schema=( is_null( $schema )?$this->defaults['schema']:$schema );
+    /**
+     * Prefixes the class metadata with the requested schema name
+     *
+     * @param ClassMetadata $classMetadata The class metadata to modify
+     */
+    public function prefix(ClassMetadata $classMetadata)
+    {
+        if ($this->defaultsSet) {
+            $blank = ('blank' == $this->defaults['schema']);
+        } else {
+            $blank = false;
         }
 
-        foreach ( $classMetadata->getAssociationMappings() as $fieldName => $mapping ) {
-            if ( $mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY ) {
-                if ( $classMetadata->associationMappings[$fieldName]['isOwningSide'] ) {
+        $bundle = strstr($classMetadata->getName(), 'Bundle\\', true) . 'Bundle';
+        $prefix = isset($this->bundles[$bundle]['prefix']) ? $this->bundles[$bundle]['prefix'] : null;
+        $schema = isset($this->bundles[$bundle]['schema']) ? $this->bundles[$bundle]['schema'] : null;
+        $flatten = isset($this->bundles[$bundle]['flatten']) ? $this->bundles[$bundle]['flatten'] : null;
+        if ($classMetadata->isIdGeneratorSequence()) {
+            $sequenceGeneratorDefinition = $classMetadata->sequenceGeneratorDefinition;
+        }
+
+        if ($this->defaults || !isset($this->bundles[$bundle])) {
+            $prefix = (is_null($prefix) ? $this->defaults['prefix'] : $prefix);
+            $schema = (is_null($schema) ? $this->defaults['schema'] : $schema);
+            $flatten = (is_null($flatten) ? $this->defaults['flatten'] : $flatten);
+        }
+
+        foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+            if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
+                if ($classMetadata->associationMappings[$fieldName]['isOwningSide']) {
                     $mappedTableName = $classMetadata->associationMappings[$fieldName]['joinTable']['name'];
-                    if ( $blank ) {
-                        if ( false !== strpos( $classMetadata->getTableName(), '.' ) ) {
+                    if ($blank) {
+                        if (false !== strpos($classMetadata->getTableName(), '.')) {
                             $classMetadata->associationMappings[$fieldName]['joinTable']['name']
-                                = str_replace( '.', '', strstr( $mappedTableName, '.' ) )
+                                = str_replace('.', '', strstr($mappedTableName, '.'))
                             ;
                         }
                     } else {
                         $classMetadata->associationMappings[$fieldName]['joinTable']['name']
                             = $prefix . $mappedTableName;
-                        if ( $schema ) {
-                            if ( false === strpos( $mappedTableName, '.' ) ) {
-                                $classMetadata->associationMappings[$fieldName]['joinTable']['name']
-                                    = $schema . '.' . $mappedTableName ;
+                        if ($schema) {
+                            if (((false === strpos($mappedTableName, '.')) && !$flatten)
+                                || ((false === strpos($mappedTableName, '___'))
+                                    && (false === strpos($mappedTableName, '.'))
+                                    && $flatten)) {
+                                if ($flatten) {
+                                    $classMetadata->associationMappings[$fieldName]['joinTable']['name']
+                                        = $schema . '___' . $mappedTableName ;
+                                } else {
+                                    $classMetadata->associationMappings[$fieldName]['joinTable']['name']
+                                        = $schema . '.' . $mappedTableName ;
+                                }
                             } else {
-                                $classMetadata->associationMappings[$fieldName]['joinTable']['name']
-                                    = $schema . strstr( $mappedTableName, '.' );
+                                if ($flatten) {
+                                    if (false === strpos($mappedTableName, '.')) {
+                                        $classMetadata->associationMappings[$fieldName]['joinTable']['name']
+                                            = $schema . strstr($mappedTableName, '___');
+                                    } else {
+                                        $classMetadata->associationMappings[$fieldName]['joinTable']['name']
+                                            = $schema . str_replace('.', '___', strstr($mappedTableName, '.'));
+                                    }
+                                } else {
+                                    $classMetadata->associationMappings[$fieldName]['joinTable']['name']
+                                        = $schema . strstr($mappedTableName, '.');
+                                }
                             }
                         }
                     }
@@ -74,30 +160,59 @@ class TablePrefixSubscriber implements \Doctrine\Common\EventSubscriber
             }
         }
 
-        if ( $blank ) {
-            if ( false !== strpos( $classMetadata->getTableName(), '.' ) ) {
-                $classMetadata->setTableName( str_replace( '.', '', strstr( $classMetadata->getTableName(), '.' ) ) );
-                $sequenceGeneratorDefinition['sequenceName']=str_replace( '.', '', strstr( $sequenceGeneratorDefinition['sequenceName'], '.' ) );
-                $classMetadata->setSequenceGeneratorDefinition( $sequenceGeneratorDefinition );
+        if ($blank) {
+            if (false !== strpos($classMetadata->getTableName(), '.')) {
+                $classMetadata->setTableName(str_replace('.', '', strstr($classMetadata->getTableName(), '.')));
+                if ($classMetadata->isIdGeneratorSequence()) {
+                    $sequenceGeneratorDefinition['sequenceName'] = str_replace('.', '', strstr($sequenceGeneratorDefinition['sequenceName'], '.'));
+                    $classMetadata->setSequenceGeneratorDefinition($sequenceGeneratorDefinition);
+                }
             }
         } else {
-            if ( false === strpos( $classMetadata->getTableName(), '.' ) ) {
-                $classMetadata->setTableName( $prefix.$classMetadata->getTableName() );
-                $sequenceGeneratorDefinition['sequenceName']=$prefix.$sequenceGeneratorDefinition['sequenceName'];
-                $classMetadata->setSequenceGeneratorDefinition( $sequenceGeneratorDefinition );
-                if ( $schema ) {
-                    $classMetadata->setTableName( $schema . '.' . $classMetadata->getTableName() );
-                    $sequenceGeneratorDefinition['sequenceName']=$schema.'.'.$sequenceGeneratorDefinition['sequenceName'];
-                    $classMetadata->setSequenceGeneratorDefinition( $sequenceGeneratorDefinition );
+            if (((false === strpos($classMetadata->getTableName(), '.')) && !$flatten) 
+                || ((false === strpos($classMetadata->getTableName(), '___'))
+                    && (false === strpos($classMetadata->getTableName(), '.')) 
+                    && $flatten)) {
+                $classMetadata->setTableName($prefix.$classMetadata->getTableName());
+                if ($schema) {
+                    if ($flatten) {
+                        $classMetadata->setTableName($schema . '___' . $classMetadata->getTableName());
+                    } else {
+                        $classMetadata->setTableName($schema . '.' . $classMetadata->getTableName());
+                    }
+                }
+                if ($classMetadata->isIdGeneratorSequence()) {
+                    $sequenceGeneratorDefinition['sequenceName'] = $prefix.$sequenceGeneratorDefinition['sequenceName'];
+                    $classMetadata->setSequenceGeneratorDefinition($sequenceGeneratorDefinition);
+                    if ($schema) {
+                        $sequenceGeneratorDefinition['sequenceName'] = $schema.'.'.$sequenceGeneratorDefinition['sequenceName'];
+                        $classMetadata->setSequenceGeneratorDefinition($sequenceGeneratorDefinition);
+                    }
                 }
             } else {
-                $classMetadata->setTableName( str_replace( '.', '.'.$prefix, $classMetadata->getTableName() ) );
-                $sequenceGeneratorDefinition['sequenceName']=str_replace( '.', '.'.$prefix, $sequenceGeneratorDefinition['sequenceName'] );
-                $classMetadata->setSequenceGeneratorDefinition( $sequenceGeneratorDefinition );
-                if ( $schema ) {
-                    $classMetadata->setTableName( $schema . strstr( $classMetadata->getTableName(), '.' ) );
-                    $sequenceGeneratorDefinition['sequenceName']=$schema.strstr( $sequenceGeneratorDefinition['sequenceName'], '.' );
-                    $classMetadata->setSequenceGeneratorDefinition( $sequenceGeneratorDefinition );
+                if ($flatten) {
+                    if (false === strpos($classMetadata->getTableName(), '.')) {
+                        $classMetadata->setTableName(str_replace('___', '___' . $prefix, $classMetadata->getTableName()));
+                    } else {
+                        $classMetadata->setTableName(str_replace('.', '___' . $prefix, $classMetadata->getTableName()));
+                    }
+                } else {
+                    $classMetadata->setTableName(str_replace('.', '.' . $prefix, $classMetadata->getTableName()));
+                }
+                if ($schema) {
+                    if ($flatten) {
+                        $classMetadata->setTableName($schema . strstr($classMetadata->getTableName(), '___'));
+                    } else {
+                        $classMetadata->setTableName($schema . strstr($classMetadata->getTableName(), '.'));
+                    }
+                }
+                if ($classMetadata->isIdGeneratorSequence()) {
+                    $sequenceGeneratorDefinition['sequenceName'] = str_replace('.', '.' . $prefix, $sequenceGeneratorDefinition['sequenceName']);
+                    $classMetadata->setSequenceGeneratorDefinition($sequenceGeneratorDefinition);
+                    if ($schema) {
+                        $sequenceGeneratorDefinition['sequenceName'] = $schema . strstr($sequenceGeneratorDefinition['sequenceName'], '.');
+                        $classMetadata->setSequenceGeneratorDefinition($sequenceGeneratorDefinition);
+                    }
                 }
             }
         }
